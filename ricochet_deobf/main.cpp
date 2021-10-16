8818c7066ee32b70fd28a6d6d0a77eb9aef647fe
weak1337 <57501119+weak1337@users.noreply.github.com>
2021-10-17 00:49:41 +0200

    Update main.cpp

--- ricochet_deobf/main.cpp
+++ ricochet_deobf/main.cpp
@@ -50,38 +50,56 @@ int main()
             char buffer[256];
             ZydisFormatterFormatInstruction(&formatter, &instruction, buffer, sizeof(buffer),
                 base + rel_func_to_deobfuscate + offset);
-            BYTE current = *(BYTE*)(base + rel_func_to_deobfuscate + offset);
-
-            /*
-                Check opcode
-            */
-            switch (current) {
-            case 0xE9: { //Jump -> combine control flow
-                uintptr_t destination = base + rel_func_to_deobfuscate + offset + *(signed int*)(base + rel_func_to_deobfuscate + offset + 1) + 5;
-                rel_func_to_deobfuscate = destination - base;
-                offset = 0x0;     
+            switch (instruction.mnemonic) {
+            case   ZYDIS_MNEMONIC_JMP: { //Jump -> combine control flow
+                if (instruction.length == 5) {
+                    uintptr_t destination = base + rel_func_to_deobfuscate + offset + *(signed int*)(base + rel_func_to_deobfuscate + offset + 1) + 5;
+                    rel_func_to_deobfuscate = destination - base;
+                    offset = 0x0;
+                }
+                else
+                {
+                    uintptr_t destination = base + rel_func_to_deobfuscate + offset + *(INT8*)(base + rel_func_to_deobfuscate + offset + 1) + 2;
+                    printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
+                    printf("%s (%x)\n", buffer, destination - base);
+                    offset += instruction.length;
+                }
                 break;
             }
-            case 0xC3: { //Return
+            case ZYDIS_MNEMONIC_JZ: {
+                uintptr_t destination = base + rel_func_to_deobfuscate + offset + *(signed int*)(base + rel_func_to_deobfuscate + offset + 2) + 6;
+                printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
+                printf("%s (%x)\n", buffer, destination - base);
+                offset += instruction.length;
+                break;
+            }
+            case ZYDIS_MNEMONIC_JNZ: {
+                uintptr_t destination = base + rel_func_to_deobfuscate + offset + *(signed int*)(base + rel_func_to_deobfuscate + offset + 2) + 6;
+                printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
+                printf("%s (%x)\n", buffer, destination - base);
+                offset += instruction.length;
+                break;
+            }
+            case ZYDIS_MNEMONIC_RET: { //Return
                 printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
                 printf("%s\n", buffer);
                 should_run = false;
                 break;
             }
-            case 0xCC: { //Function end or breakpoint
+            case ZYDIS_MNEMONIC_INT3: { //Function end or breakpoint
                 printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
                 printf("%s\n", buffer);
                 should_run = false;
                 break;
             }
-            case 0xCD: { //0x29 -> fastfail
+            case ZYDIS_MNEMONIC_INT: { //Interrupt
                 should_run = false;
                 printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
                 printf("%s\n", buffer);
                 break;
             }
-            case 0xFF: {
-                if (*(BYTE*)(base + rel_func_to_deobfuscate + offset + 1) == 0x15) { //Call to import
+            case ZYDIS_MNEMONIC_CALL: {
+                if (instruction.length == 6) { //Call to import
                     printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
                     uintptr_t address = base + rel_func_to_deobfuscate + offset;
                     DWORD relative = *(DWORD*)(address + 2) + 6;
@@ -90,51 +108,15 @@ int main()
                     offset += instruction.length;
                 }
                 else {
-                    printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
-                    printf("%s\n");
-                    offset += instruction.length;
-                }
-                break;
-            }
-            case 0xE8: { //Call resolve rva
-                uintptr_t destination = base + rel_func_to_deobfuscate + offset + *(signed int*)(base + rel_func_to_deobfuscate + offset + 1) + 5;
-                printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
-                printf("%s (%x)\n", buffer, destination - base);
-                offset += instruction.length;
-                break;
-            }
-            case 0xEB: { //Short jump resolve rva
-                uintptr_t destination = base + rel_func_to_deobfuscate + offset +  *(INT8*)(base + rel_func_to_deobfuscate + offset + 1)  + 2;
-                printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
-                printf("%s (%x)\n", buffer, destination - base);
-                offset += instruction.length;
-                break;
-            }
-            case 0x74: { //cond jump
-                uintptr_t destination = base + rel_func_to_deobfuscate + offset + *(INT8*)(base + rel_func_to_deobfuscate + offset + 1) + 2;
-                printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
-                printf("%s (%x)\n", buffer, destination - base);
-                offset += instruction.length;
-                break;
-            }
-            case 0x0F: { //cond jump
-                if (*(BYTE*)(base + rel_func_to_deobfuscate + offset + 1) == 0x84 || *(BYTE*)(base + rel_func_to_deobfuscate + offset + 1) == 0x85) {
-                    uintptr_t destination = base + rel_func_to_deobfuscate + offset + *(signed int*)(base + rel_func_to_deobfuscate + offset + 2) + 6;
+                    uintptr_t destination = base + rel_func_to_deobfuscate + offset + *(signed int*)(base + rel_func_to_deobfuscate + offset + 1) + 5;
                     printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
                     printf("%s (%x)\n", buffer, destination - base);
                     offset += instruction.length;
                 }
-                else //Rdstc
-                {
-                    printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
-                    printf("%s\n", buffer);
-                    offset += instruction.length;
-
-                }
                 break;
             }
             default: {
-                if (current != 0x90) { //Other instruction / ignore nops
+                if (instruction.mnemonic != ZYDIS_MNEMONIC_NOP) { //Other instruction / ignore nops
                     printf("%016" PRIX64 " -> ", base + rel_func_to_deobfuscate + offset);
                     printf("%s\n", buffer);
                 }
@@ -142,7 +124,7 @@ int main()
                 offset += instruction.length;
                 break;
             }
-            }
+            }      
         }
     }
     system("pause");
